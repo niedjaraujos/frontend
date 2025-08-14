2786bae7175dd563bb98643538bd9eacf4b1cca3
niedjaraujos <naraujos1007@hotmail.com>
2025-08-14 16:50:42 -0300

    footer commit

--- src/components/layout/header-search.tsx
+++ src/components/layout/header-search.tsx
@@ -2,7 +2,8 @@ export const HeaderSearch = () => {
   return (
     <input
       type="search"
-      className="border border-gray-200 w-full pl-12 py-3 rounded-sm outline-0 bg-no-repeat bg-[url(/assets/ui/search.png)] bg-[16px_50%] bg-[size:24px]"
+      className="border border-gray-200 w-full pl-12 py-3 rounded-sm
+       outline-0 bg-no-repeat bg-[url(/assets/ui/search.png)] bg-[16px_50%] bg-[size:24px]"
       placeholder="O que você procura?"
       /* style={{ backgroundImage: 'url(/assets/ui/search.png)' }}*/
     />
